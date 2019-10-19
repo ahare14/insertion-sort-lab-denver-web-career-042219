30ef4794e56558f636cb9409a416699f7c4b1173
Alex Hare <alexanderhare14@gmail.com>
2019-10-19 00:00:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 function findMinAndRemove(array){
-
+  let min = Math.min.apply(null, array);
+  array.splice(array.indexOf(min));
+  return array;
 }
 
 function insertionSort(array){
-
+  let len = array.length;
+  for(let i = 1; i < len; i++){
+    let j = i - 1;
+    let elem = array[i];
+    while(j >= 0 && array[j] > elem){
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = elem;
+  }
+  return array;
 }
